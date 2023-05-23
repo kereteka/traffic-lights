520c09a934151523778410db327adeac41e59f81
kereteka <mehmet.sengun1@gmail.com>
2023-05-23 11:43:28 +0300

    final

--- src/components/Screen/index.jsx
+++ src/components/Screen/index.jsx
@@ -1,21 +1,28 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useMemo, useState } from "react";
 import TrafficLight from "../TrafficLights";
 
 const Screen = () => {
   const [open, setOpen] = useState(0);
   const [isOpen, setIsOpen] = useState(false);
-  const [greenTime, setGreenTime] = useState(); // Varsayılan yeşil süre 10 saniye
+  const [isYellow, setIsYellow] = useState(false);
+  const [isRed, setIsRed] = useState(false);
+  const [greenTime, setGreenTime] = useState(5);
   const [timer, setTimer] = useState(5);
+  const [input, setInput] = useState();
+  const [buttonDisabled, setButtonDisabled] = useState(false);
 
   const defaultTime = 5;
-  const color = [
-    ["red", "red", defaultTime],
-    ["green", "red", greenTime],
-    ["yellow", "red", defaultTime],
-    ["red", "green", greenTime],
-    ["red", "yellow", defaultTime],
-    ["red", "red", defaultTime],
-  ];
+  const color = useMemo(
+    () => [
+      ["red", "red", defaultTime],
+      ["green", "red", greenTime],
+      ["yellow", "red", defaultTime],
+      ["red", "green", greenTime],
+      ["red", "yellow", defaultTime],
+      ["red", "red", defaultTime],
+    ],
+    [defaultTime, greenTime]
+  );
 
   const changeInterval = (time) =>
     !isOpen &&
@@ -25,7 +32,6 @@ const Screen = () => {
 
   useEffect(() => {
     const interval = changeInterval(color[open][2] * 1000);
-
     !isOpen ? setTimer(color[open][2]) : setTimer(15);
 
     const countdown = setInterval(() => {
@@ -38,18 +44,40 @@ const Screen = () => {
     };
   }, [open, isOpen, greenTime]);
 
-  const handleClick = () => {
-    setIsOpen(true);
-    setOpen(0);
-    setTimeout(() => {
-      setIsOpen(false);
-    }, 15000);
-  };
+  const handleClick = useCallback(() => {
+    if (!buttonDisabled) {
+      setIsOpen(true);
+      setIsYellow(true);
+      setButtonDisabled(true);
 
-  const handleInputChange = (value) => {
+      setTimeout(() => {
+        setIsYellow(false);
+        setIsRed(true);
+      }, 7500);
+
+      setTimeout(() => {
+        setIsRed(false);
+      }, 15000);
+
+      setOpen(0);
+      setTimeout(() => {
+        setIsOpen(false);
+        setButtonDisabled(false);
+      }, 15000);
+    }
+  }, [buttonDisabled]);
+
+  const handleInputChange = useCallback((value) => {
+    value = Math.floor(value);
+    if (value > 30) {
+      value = 30;
+    } else if (value < 1) {
+      value = 1;
+    }
+
     setGreenTime(value);
-  };
-
+    setInput(value);
+  }, []);
   return (
     <div className="w-fit flex flex-col items-center mx-auto">
       <div className=" w-full mt-24 -mb-12 bg-gray-400 flex justify-between px-24">
@@ -57,22 +85,28 @@ const Screen = () => {
         <div>Sonraki Adıma Kalan Süre : {timer}</div>
       </div>
       <div className="flex justify-center items-center">
-        <TrafficLight active={color[open][0]} />
-        <TrafficLight active={color[open][1]} />
+        <TrafficLight
+          active={isYellow ? "yellow" : isRed ? "red" : color[open][0]}
+        />
+        <TrafficLight
+          active={isYellow ? "yellow" : isRed ? "red" : color[open][1]}
+        />
         <TrafficLight active={isOpen ? "green" : "red"} />
         <input
           className=" border border-black h-12 w-24 p-4"
           type="number"
           max={30}
           min={1}
+          step={1}
+          value={input}
           onChange={(e) => handleInputChange(e.target.value)}
         />
         <button
           className=" w-12 h-12 ml-4 bg-green-400 rounded-full border-4 border-black hover:brightness-95"
           onClick={handleClick}
+          disabled={buttonDisabled}
         ></button>
       </div>
-      <input type="text" />
     </div>
   );
 };
--- src/components/TrafficLights/index.jsx
+++ src/components/TrafficLights/index.jsx
@@ -1,42 +1,7 @@
 import React from "react";
 
 const TrafficLight = ({ active }) => {
-  // const [open, setOpen] = useState(0);
   const lights = ["red", "yellow", "green"];
-
-  // const [greenTime, setGreenTime] = useState(0); // Varsayılan yeşil süre 10 saniye
-  // const [timer, setTimer] = useState(5);
-
-  // const defaultTime = 5;
-
-  // const color = [
-  //   ["red", "red", defaultTime],
-  //   ["green", "red", greenTime],
-  //   ["yellow", "red", defaultTime],
-  //   ["red", "green", greenTime],
-  //   ["red", "yellow", defaultTime],
-  //   ["red", "red", defaultTime],
-  // ];
-  // const grOne = ["red", "green", "yellow", "red", "red", "red"];
-  // const grTwo = ["red", "red", "red", "green", "yellow", "red"];
-
-  //const [timer, setTimer] = useState(5); // Timer set to 5 seconds
-
-  // useEffect(() => {
-  //   const interval = setInterval(() => {
-  //     setOpen((prevOpen) => (prevOpen + 1) % lights.length);
-  //     setTimer(5); // Reset the timer to 5 seconds for each light
-  //   }, 5000); // Open the next light every 5 seconds
-
-  //   const countdown = setInterval(() => {
-  //     setTimer((prevTimer) => prevTimer - 1);
-  //   }, 1000);
-
-  //   return () => {
-  //     clearInterval(interval);
-  //     clearInterval(countdown);
-  //   };
-  // }, []);
 
   return (
     <div className="p-24 w-24 flex flex-col items-center">
